1acd025be6854f5aa6e3dba92dd751a1b2eea05b
NotPerr <vovo7292@gmail.com>
2023-02-24 17:17:34 +0800

    added search component

---
+++ src/api.js
@@ -0,0 +1,10 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '5574a3e6femsh159fd254f0aebc4p12f646jsn8401e48d8af7',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+

6fb421fc0f83f0c20bdf0be8d36ab63613b116d3
NotPerr <vovo7292@gmail.com>
2023-02-25 19:56:10 +0800

    fetch current weather data

--- src/api.js
+++ src/api.js
@@ -8,3 +8,6 @@ export const geoApiOptions = {
 
 export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+
+export const WEATHER_API_KEY = "c498b70130b9622c8f796a9f24aa8889";
